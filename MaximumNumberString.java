62c0793f4350b3d2151e1b477e23d36582ce6a6e
Sangeethaposam <120437076+Sangeethaposam@users.noreply.github.com>
2023-01-31 15:57:50 +0530

    Find which is maximum in given string

---
+++ MaximumNumberString.java
@@ -0,0 +1,23 @@
+package com.bridzelabz.maximumNumbers;
+
+public class MaximumNumberString {
+	static void max_variables(String str1, String str2, String str3){
+        if(str1.compareTo(str2) > 0){
+            if(str1.compareTo(str3) > 0){
+                System.out.println( str1 + " is Maximum of three strings ");
+            }else {
+                System.out.println( str3 + " is Maximum of three strings ");
+            }
+        }else{
+            if(str2.compareTo(str3) > 0){
+                System.out.println( str2 + " is Maximum of three strings ");
+            }else{
+                System.out.println( str3 + " is Maximum of three strings ");
+            }
+        }
+    }
+	public static void main(String[] args) {
+		max_variables("Apple","Peach","Banana");
+	}
+
+}
